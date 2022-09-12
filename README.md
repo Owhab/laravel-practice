d7e8aefbb6006070feb7d48583434480e920653d
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-09-12 13:30:37 +0600

    loading view page using controller

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Route::get('/test', function () {
     return view('test');
 });
 ```
+
+## Loading view pages from controller
+    - First need to create a controller by using this command ```php artisan make:controller DemoController```
+    - Then need to call it from router ```web.php``` file like this 
+    ```Route::get("/demo",[DemoController::class, 'Index']);
+    ``` In this route, The ```Index``` refers the ```Index``` function in the controller
+    - From ```Index``` function of ```DemoController.php``` need to view the ```demo.php``` view; 
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\DemoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,5 +11,9 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+// Loading controllers from controllers folder
+
+Route::get("/demo", [DemoController::class, 'Index']);
 
 Route::get("/projects", "App\Http\Controllers\ProjectController@index");
